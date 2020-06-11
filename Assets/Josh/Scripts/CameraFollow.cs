8df03398288ce8b35debf9decb3c32f2b64a75ef
Josh Nelson <josh.nelson1@yahoo.com>
2020-06-11 02:56:09 -0600

    Josh's Changes

---
+++ Assets/Josh/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float moveSpeed;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetOffsetPos = target.position + offset;
+        transform.position = Vector3.LerpUnclamped(transform.position, targetOffsetPos, Time.deltaTime * moveSpeed);        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(target.position, target.position + offset);
+    }
+
+}
---
+++ Assets/Josh/Scripts/FireBullet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnLocation;
+    public float force;
+    public float fireRate;
+    private float fireCooldown;
+    private float maxFireTime;
+    private bool canFire = false;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        calculateFireRate();        
+    }
+
+    public void calculateFireRate()
+    {
+        maxFireTime = 1 / fireRate;
+    }
+    public void OnValidate()
+    {
+        calculateFireRate();
+    }
+    // Update is called once per frame
+    void Update()
+    {   
+
+        if(fireCooldown>0)
+        {
+            fireCooldown -= Time.deltaTime;
+            canFire = false;
+        }else
+        {
+            canFire = true;
+        }
+
+        if(Input.GetKey(KeyCode.Space) && canFire)
+        {
+            fireBullet();
+        }
+    }
+
+    void fireBullet()
+    {
+        GameObject newBullet = Instantiate(bulletPrefab, bulletSpawnLocation.position, bulletSpawnLocation.rotation);
+        Rigidbody rb = newBullet.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * force);
+
+        fireCooldown = maxFireTime;
+    }
+
+}
