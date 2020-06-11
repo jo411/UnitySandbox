33c163d17c75bfd793d91a0fa03148f2d356d732
Unknown <xcseage@gmail.com>
2020-06-11 03:03:31 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Casey/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float moveSpeed;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 targetOffPos = target.position + offset;
+
+        transform.position = Vector3.LerpUnclamped(transform.position, targetOffPos, Time.deltaTime * moveSpeed);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(target.position, target.position + offset);
+    }
+}
---
+++ Assets/Casey/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+        Vector3 movement = Vector3.zero;
+
+        if (hAxis != 0)
+        {
+            movement.x = Time.deltaTime * moveSpeed * Mathf.Sign(hAxis);
+        }
+
+        if (vAxis != 0)
+        {
+            movement.z = Time.deltaTime * moveSpeed * Mathf.Sign(vAxis);
+        }
+
+        transform.position += movement;
+      
+    }
+}
