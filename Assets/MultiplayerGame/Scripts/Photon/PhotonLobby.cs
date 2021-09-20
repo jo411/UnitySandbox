b4c57ae9a202796fc75e02e82ea1132f682d55a2
Josh Nelson <josh.nelson1@yahoo.com>
2021-09-19 19:18:58 -0600

    First Video content

---
+++ Assets/MultiplayerGame/Scripts/Photon/PhotonLobby.cs
@@ -0,0 +1,74 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+      
+    public GameObject battleButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        lobby = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        battleButton.SetActive(true);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Player failed to join random room");
+        CreateRoom();
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.LogError("Failed to create room: " + message);
+        CreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Player joined room");
+    }
+
+    public void OnBattleButtonClicked()
+    {
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();          
+    }
+
+    void CreateRoom()
+    {
+        System.Guid uuid = System.Guid.NewGuid();
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 10 };
+        PhotonNetwork.CreateRoom("Room" + uuid.ToString(), roomOps);
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+
+}
